4b00be114cc1b93f9a742159823962ed811d1af3
Lucky <lucky_soft@163.com>
2024-06-06 17:44:24 +0800

    init project

---
+++ app/components/dashboards.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { HotNews } from "./dashboards/hot-news";
+
+export function Dashboards() {
+  return (
+    <div>
+      <h1>Dashboards</h1>
+      <HotNews />
+    </div>
+  );
+}
